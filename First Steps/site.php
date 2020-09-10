80622dfaf992c1715a721e36980181f9581137f2
Csaba Puskás <36250394+csabi333@users.noreply.github.com>
2020-09-10 15:39:17 +0200

    very basic calculator

--- First Steps/site.php
+++ First Steps/site.php
@@ -76,5 +76,17 @@
     echo $_GET["Username"];
     echo $_GET["age"];
     ?>  
+
+<br>
+add these two: <br>
+<form action="site.php" method="get">
+        Name:<input type="number" name="num1"><br>
+        Age:<input type="number" name="num2">
+        <input type="submit">
+    </form> 
+Answer:
+    <?php 
+    echo $_GET["num1"] + $_GET["num2"];
+    ?>
 </body>
 </html>
