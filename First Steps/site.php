ef8fc7ffa00864ac30529604a9ae33b7234b0cde
Csaba Puskás <36250394+csabi333@users.noreply.github.com>
2020-09-10 14:58:02 +0200

    data types

--- First Steps/site.php
+++ First Steps/site.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php 
+
     $characterName = "John";
     $characterAge = 70;
     echo("there once was a man name $characterName<br>");
@@ -14,6 +15,13 @@
     $characterName ="Terry";
     echo("he liked the name $characterName<br>");
     echo("he didnt like being $characterAge<br>");
+
+    $phrase = "To be or not to be<br>"; //string
+    $age = 30; //number, integer
+    $float = 3.7; //decimal
+    //difference between 30 and 30.0
+    $bool = true;
+    echo $phrase;
     ?>
 </body>
 </html>
